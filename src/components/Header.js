6a2896a69455d18392346e433efb9741a89f04df
Darrel Belvin <darrel.belvin@gmail.com>
2019-04-29 19:10:46 -0700

    Some edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import logo from '../images/DDD Logo Color Flat.png'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-square"></span>
+            <span><img src={logo} alt="" width="100"/></span>
         </div>
         <div className="content">
             <div className="inner">
@@ -14,7 +15,7 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Introff</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
