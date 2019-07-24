f5a326ed7e6e531f1caaad846922c193d1f1fff0
Darrel Belvin <darrel.belvin@gmail.com>
2019-07-24 15:37:58 -0700

    Mostly finished, checking contact form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import * as emailjs from 'emailjs-com';
 
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
+import pic01 from '../images/Binary Code.jpg'
+import pic02 from '../images/IMG_20190724_115926_small.jpg'
 import pic03 from '../images/pic03.jpg'
 
 import cabDes1 from '../images/cabDes1.png'
@@ -22,9 +23,9 @@ const imageSets = {
   elevators: [cabDes1, cabDes2, cabDes3, cabDes4, cabDes5, cabDes6]
 }
 
-const WorkIcon = () => <svg class="jss1" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"></path></svg>
-const SchoolIcon = () => <svg class="jss1" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"></path></svg>
-const StarIcon = () => <svg class="jss1" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path></svg>
+const WorkIcon = () => <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"></path></svg>
+const SchoolIcon = () => <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"></path></svg>
+const StarIcon = () => <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path></svg>
 
 class Main extends React.Component {
   constructor(props) {
@@ -35,6 +36,23 @@ class Main extends React.Component {
       photoIndex: 0,
       isLightboxOpen: false,
     };
+    console.log("asdfkljhasdklfjh")
+    console.log("%REACT_APP_EMAILJS_USERID%");
+    console.log(process.env.REACT_APP_EMAILJS_USERID);
+    emailjs.init(process.env.REACT_APP_EMAILJS_USERID);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(process.env.REACT_APP_EMAILJS_SERVICEID);
+
+    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICEID, process.env.REACT_APP_EMAILJS_TEMPLATEID, '#contact_form')
+      .then(function(response) {
+         console.log('SUCCESS!', response.status, response.text);
+      }, function(error) {
+         console.log('FAILED...', error);
+      });
   }
 
   render() {
@@ -49,18 +67,18 @@ class Main extends React.Component {
           <h2 className="major">Intro</h2>
           <span className="image main"><img src={pic01} alt="" /></span>
           <p>Darrel is my name and I love building stuff. I've been challenging myself to create more impressive things since before I can remember.
-          My older brother introduced me to programming at age 13 and I've been tinkering with scripts and applications ever since, over half my life now.<br/>
+          My older brother introduced me to programming at age 13 and I've been making scripts and applications ever since, over half my life now.<br/>
           Being self taught and a fiercely independent thinker gives me an advantage in situations where creative, out of the box thinking is required.
           If you're looking for a fresh perspective or unique solution, I'm your man.
-          I love puzzles (I collect them), challenges, and beautiful solutions.</p> {// eslint-disable-next-line
+          I love puzzles, challenges, and beautiful solutions.</p> {// eslint-disable-next-line
 }         <p>Check out <a onClick={() => this.props.onOpenArticle('work')}>my work</a>.</p>
           {close}
         </article>
 
         <article ref={(this.props.article === 'work' && !isLightboxOpen) && this.props.setWrapperRef} id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none', width: '1400px'}}>
           <h2 className="major">Work</h2>
-          <p>I have been tinkering with scripts and applications for over 13 years and have used 16 different programming languages, all self-taught.</p>
-          <p>This is a sampling of some of my larger projects over the years. </p>
+          <p>I have been building scripts and applications for over 13 years and have used 16 different programming languages, all self-taught. With the speed at which I am able to learn new API's and programming languages, not having experience with a technology is not a barrier to tackling a project utilizing it.</p>
+          <h3>These are of some of my larger projects over the years</h3>
           
           <VerticalTimeline>
             <VerticalTimelineElement
@@ -73,7 +91,7 @@ class Main extends React.Component {
                 <strong>Problem:</strong> Competition offers online interior designers and Vertical Dimensions did not.<br/>
                 <strong>Solution:</strong> A website module that designs and renders elevator interiors live in 3D. Vertical Dimensions now has the most advanced elevator interior designer in existence. <a href="http://qcdesignerbeta.netlify.com" target="_blank" rel="noopener noreferrer">Go check it out.</a>
               </p>
-              <img className="image main" style={{maxWidth: "500px"}} src={cabThumbs} alt="error" onClick ={() => this.setState({ isLightboxOpen: true})} />
+              <img className="image gallery" src={cabThumbs} alt="error" onClick ={() => this.setState({ isLightboxOpen: true})} />
             </VerticalTimelineElement>
             <VerticalTimelineElement
               date="2018"
@@ -104,8 +122,8 @@ class Main extends React.Component {
               <h3 className="vertical-timeline-element-title">Engineering Software Extension</h3>
               <h4 className="vertical-timeline-element-subtitle">DHP Engineering, Federal Way, WA</h4>
               <p>
-                <strong>Problem:</strong> Finite element analysis software unable to solve for data needed for customer requested design tables. Guess and check methods will take weeks to run.<br/>
-                <strong>Solution:</strong> Extend software capabilities using QuickMacros to solve.............
+                <strong>Problem:</strong> Finite element analysis software unable to solve for data needed in customer requested design tables. Guess and check methods will take weeks to run.<br/>
+                <strong>Solution:</strong> Extend software capabilities using QuickMacros to automatically run guess and check method, turning weeks into days, then integrate it into Excel to automate data entry, turning days into a single unattended hour.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -115,8 +133,9 @@ class Main extends React.Component {
               <h3 className="vertical-timeline-element-title">Drafting Tools</h3>
               <h4 className="vertical-timeline-element-subtitle">DHP Engineering, Federal Way, WA</h4>
               <p>
-                <strong>Problem:</strong> <br/>
-                <strong>Solution:</strong> 
+                <strong>Problem:</strong> Repetitive drafting tasks are tedious and time consuming.<br/>
+                <strong>Solution:</strong> Dozens of LISP and VBA scripts to speed up drafting and automate tasks, including a script that helps create scripts.<br/>
+                <strong>New Problem:</strong> Small engineering firm can no longer keep me busy.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -126,8 +145,7 @@ class Main extends React.Component {
               <h3 className="vertical-timeline-element-title">Tracing Keplers Path with Mathematica</h3>
               <h4 className="vertical-timeline-element-subtitle">Highline College, Des Moines, WA</h4>
               <p>
-                <strong>Problem:</strong> <br/>
-                <strong>Solution:</strong> 
+                <strong>Honors Project:</strong> Create an accurate model of the solar system in Mathematica. Presented project on campus and at regional mathematics conference.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -137,8 +155,7 @@ class Main extends React.Component {
               <h3 className="vertical-timeline-element-title">Pac-Man Remake</h3>
               <h4 className="vertical-timeline-element-subtitle">Highline College, Des Moines, WA</h4>
               <p>
-                <strong>Problem:</strong> <br/>
-                <strong>Solution:</strong> 
+                <strong>Honors Project:</strong> Create a re-make of the classic game Pac-Man in Java.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -147,8 +164,9 @@ class Main extends React.Component {
             >
               <h3 className="vertical-timeline-element-title">Skin.rb</h3>
               <p>
-                <strong>Problem:</strong> <br/>
-                <strong>Solution:</strong> 
+                <strong>Problem:</strong> Stitching together profiles in SketchUp, a 3D modeling program, was tedious and time consuming in 2007.<br/>
+                <strong>Solution:</strong> A Ruby plugin that automates said stitching process.<br/>
+                <strong>Community Response:</strong>Very impressed that a <a href="https://sketchucation.com/forums/viewtopic.php?f=323&t=1306#p5850" target="_blank" rel="noopener noreferrer"> 14 year old</a> created such a useful tool.<br/>
               </p>
             </VerticalTimelineElement>
             
@@ -157,10 +175,10 @@ class Main extends React.Component {
         </article>
 
         <article ref={this.props.article === 'play' && this.props.setWrapperRef} id="play" className={`${this.props.article === 'play' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Play</h2>
+          <h2 className="major">Hobbies</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>I love playing with puzzles, tennis, and minecraft.<br/>
-          I'm a huge fan of science and futurism, AI, rockets, and astronomy.<br/>
+          <p>I enjoy playing tennis, solving puzzles, and making art.<br/>
+          I'm a huge fan of science and futurism, artificial inteligence, rockets, and astronomy.<br/>
           I'm learning guitar and re-learning piano.</p>
           {close}
         </article>        
@@ -174,21 +192,21 @@ class Main extends React.Component {
 
         <article ref={this.props.article === 'contact' && this.props.setWrapperRef} id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form id="contact_form">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="email" id="email" required/>
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" maxLength="6000" required></textarea>
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
+              <li><input type="submit" onClick={this.handleSubmit} value="Send Message" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
             </ul>
           </form>
